98b3424766cced479d41f267eed5ca924ebbad5f
Godsgift Akari <giftakari@yahoo.com>
2019-01-22 22:13:54 +1100

    Changed Js Folder Name in .HTML file

--- Js/main.js
+++ Js/main.js
@@ -13,7 +13,7 @@ weatherConditions.send(null);
 weatherConditions.onload = function() {
     if (weatherConditions.status === 200){
         cObj = JSON.parse(weatherConditions.responseText); 
-        console.log(cObj);
+        
         document.getElementById('location').innerHTML =`${cObj.name}`;
         document.getElementById('weather').innerHTML = cObj.weather[0].description;
         document.getElementById('temperature').innerHTML =`${cObj.main.temp}&degF`;
@@ -46,7 +46,7 @@ weatherForecast.send();
 weatherForecast.onload = function() {
 if (weatherForecast.status === 200){
 	fObj = JSON.parse(weatherForecast.responseText);
-    console.log(fObj);
+   
     var date_raw = fObj.list[0].dt_txt;
     date_raw =date_raw.substring(0,11);
 document.getElementById('r1c1').innerHTML = date_raw;
@@ -61,8 +61,8 @@ document.getElementById('r1c3').innerHTML =`${fObj.list[0].main.temp_min} &degF`
 document.getElementById('r1c4').innerHTML = `${fObj.list[0].main.temp_max} &degF`;
 
 //Next Day weather forecast
-console.log(fObj);
-    var date_raw = fObj.list[8].dt_txt;
+
+     date_raw = fObj.list[8].dt_txt;
     date_raw =date_raw.substring(0,11);
 document.getElementById('r2c1').innerHTML = date_raw;
 
@@ -76,7 +76,7 @@ document.getElementById('r2c3').innerHTML =`${fObj.list[8].main.temp_min} &degF`
 document.getElementById('r2c4').innerHTML = `${fObj.list[8].main.temp_max} &degF`;
 
 // Next Day Weather Forecast
-console.log(fObj);
+
      date_raw = fObj.list[16].dt_txt;
     date_raw =date_raw.substring(0,11);
 document.getElementById('r3c1').innerHTML = date_raw;
